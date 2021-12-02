3edcdb02640a2196b6df3f3690947b3b9fffc6b2
Javier <jmartin@grupoloyal.com>
2021-12-02 19:32:10 +0100

    Soluciones

---
+++ ReactiveSpringCliente/src/main/java/com/example/ReactiveSpringClienteApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringClienteApplication.class, args);
+	}
+
+}
